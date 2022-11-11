b62dc50d6d69ffc9aeb156272c687ccaf96751a2
Astro_815 <103153597+Astro815@users.noreply.github.com>
2022-11-11 13:24:29 -0300

    Add files via upload

---
+++ GameCar/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1668183813,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/divição-sheet0.png",
+		"images/p1.png",
+		"images/sprite4-sheet0.png",
+		"images/tiledbackground.png",
+		"images/marca_player-sheet0.png",
+		"images/marca_player-sheet1.png",
+		"images/marca_player-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
